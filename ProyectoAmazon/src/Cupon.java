b88dc8e0acc93413cd3583db202e1bf0b5e85735
Angycht <angytc98@gmail.com>
2024-02-11 12:24:44 +0100

    Funcion quitar saldo y funcion fusionar cupones

--- ProyectoAmazon/src/Cupon.java
+++ ProyectoAmazon/src/Cupon.java
@@ -33,7 +33,24 @@ public class Cupon {
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+	//Metodos
+	 public void gastarSaldo(double cantidad) {
+	        if (cantidad > saldo) {
+	            throw new IllegalArgumentException("Error: No hay suficiente saldo en el cupón.");
+	        } else {
+	            saldo -= cantidad;
+	            
+	        }
+	    }
+
+	    // Método para fusionar dos cupones
+	    public static Cupon fusionarCupones(Cupon cupon1, Cupon cupon2) {
+	        double nuevoSaldo = cupon1.getSaldo() + cupon2.getSaldo();
+	        Cupon cuponNuevo = new Cupon(nuevoSaldo);
+	        cupon1.setSaldo(0);
+	        cupon2.setSaldo(0);
+	        return cuponNuevo;
+	    }
 	
 	
 	
