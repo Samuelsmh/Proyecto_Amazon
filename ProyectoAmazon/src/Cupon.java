5b97e36d41562e00a0367f7bae2562145c219651
Angycht <angytc98@gmail.com>
2024-02-13 18:13:32 +0100

    id auto incremental

--- ProyectoAmazon/src/Cupon.java
+++ ProyectoAmazon/src/Cupon.java
@@ -3,17 +3,15 @@ import java.util.Random;
 public class Cupon {
 	
 	// Atributos
-	private int identificador;
+	private int identificador =10000;
 	private double saldo;
-	private static int identificadorAutoIncrement = 10000;
+	//private int identificadorAutoIncrement = 10000;
 	
-	private static Random random = new Random();
+	//private static Random random = new Random();
 	//Constructor
 	public Cupon(double saldo) {
-		
-		
-		//Generamos un random de esta manera. Así el número del ID sera aleatorio, pero será de 5 digitos
-		this.identificador = random.nextInt(90000) + 10000;
+	
+		this.identificador++;
 		this.saldo=saldo;
 		
 	}
