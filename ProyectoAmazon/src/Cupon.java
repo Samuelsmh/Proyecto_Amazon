e2bd2949a03ab543e38bd9bc8c9fc5b65ed42507
dmarlop <davidmarinlpz@gmail.com>
2024-02-17 10:42:46 +0100

    Añadidos atributos de copiar valores (para mantenerlos intanctos) añadido método mostrar información, he puesto como auto increment el ID

--- ProyectoAmazon/src/Cupon.java
+++ ProyectoAmazon/src/Cupon.java
@@ -3,16 +3,22 @@ import java.util.Random;
 public class Cupon {
 	
 	// Atributos
-	private int identificador =10000;
+	private int identificador;
 	private double saldo;
-	//private int identificadorAutoIncrement = 10000;
+	private static int identificadorAutoIncrement = 10000;
+	private int movimientoCuponAñadir;
+	private int movimientoCuponGastar;
+	private double saldoInicial;
+	private double saldoAlAñadir;
 	
 	//private static Random random = new Random();
 	//Constructor
 	public Cupon(double saldo) {
-	
-		this.identificador++;
+		
+		
+		this.identificador = identificadorAutoIncrement++;
 		this.saldo=saldo;
+		this.saldoInicial = saldo;
 		
 	}
 
@@ -27,20 +33,59 @@ public class Cupon {
 	public double getSaldo() {
 		return this.saldo;
 	}
+	
+	public double getSaldoInicial() {
+		return this.saldoInicial;
+	}
+	
+	public double getSaldoAlAñadir() {
+		return this.saldoAlAñadir;
+	}
 
 	public void setSaldo(double saldo) {
+		if(saldo<0) {
+			 throw new IllegalArgumentException("Error: No puedes obtener un cupón de dinero negativo.");
+			
+		} 
 		this.saldo = saldo;
+		
+		}
+	
+	
+	
+	public int getMovimientoCuponAñadir() {
+		return movimientoCuponAñadir;
 	}
-<<<<<<< HEAD
+
+	
+
+	public int getMovimientoCuponGastar() {
+		return movimientoCuponGastar;
+	}
+
+	
+
+	public void añadirSaldo(double saldo) {
+		if(saldo<0) {
+			 throw new IllegalArgumentException("Error: No puedes obtener un cupón de dinero negativo.");
+			
+		} 
+		this.saldo += saldo;
+		movimientoCuponAñadir++;
+		this.saldoAlAñadir = this.saldo;
+		}
+	
+
 	//Metodos
-	 public void gastarSaldo(double cantidad) {
+	 public void gastarSaldo1(double cantidad) {
 	        if (cantidad > saldo) {
 	            throw new IllegalArgumentException("Error: No hay suficiente saldo en el cupón.");
-	        } else {
+	        } 
 	            saldo -= cantidad;
-	            
+	            System.out.println("Se han gastado " + cantidad + "€ del cupón.");
+	            movimientoCuponGastar++;
 	        }
-	    }
+	    
 
 	    // Método para fusionar dos cupones
 	    public static Cupon fusionarCupones(Cupon cupon1, Cupon cupon2) {
@@ -48,9 +93,38 @@ public class Cupon {
 	        Cupon cuponNuevo = new Cupon(nuevoSaldo);
 	        cupon1.setSaldo(0);
 	        cupon2.setSaldo(0);
+	        
 	        return cuponNuevo;
 	    }
-=======
+	    
+	    public static void mostrarInformacion(Cupon cupon) {
+	    	System.out.println("Esta es la información de tu cupón");
+	    	System.out.println("-----------------------------------");
+	    	System.out.printf("Saldo inicial: %.2f \n", cupon.getSaldoInicial());
+	    	System.out.printf("Identificador: %d \n", cupon.getIdentificador());
+	    	System.out.println("-----------------------------------");
+	    	System.out.println("Estos son tus movimientos");
+	    	System.out.println("-----------------------------------");
+	    	if(cupon.getMovimientoCuponAñadir()!= 0) {
+	    		System.out.printf("Has añadido dinero %d veces. \n", cupon.getMovimientoCuponAñadir());	
+	    		System.out.printf("Dinero resultante %.2f \n", cupon.getSaldoAlAñadir());	
+	    	} else {
+	    	System.out.printf("No has añadido dinero %d. \n", cupon.getMovimientoCuponAñadir());
+	    	}
+	    	
+	    	if(cupon.getMovimientoCuponGastar()!= 0) {
+	    		System.out.printf("Has gastado dinero %d veces. \n", cupon.getMovimientoCuponGastar());	
+	    		System.out.printf("Dinero resultante %.2f \n", cupon.getSaldo());	
+	    	} else {
+	    	System.out.printf("No has gastado dinero \n", cupon.getMovimientoCuponGastar());
+	    	}
+	    	
+	    	
+	    }
+	    
+	    
+	    
+/*
 	// Metodo para gastar saldo
 	 public void gastarSaldo(double cantidad) {
         if (cantidad > saldo) {
@@ -60,16 +134,18 @@ public class Cupon {
             System.out.println("Se han gastado " + cantidad + "€ del cupón.");
         }
     }
-
+*/
+	    
+	    /*
     // Método para fusionar dos cupones
-    public static Cupon fusionarCupones(Cupon cupon1, Cupon cupon2) {
+    public static Cupon fusionarCupones1(Cupon cupon1, Cupon cupon2) {
         double nuevoSaldo = cupon1.getSaldo() + cupon2.getSaldo();
         Cupon cuponNuevo = new Cupon(nuevoSaldo);
         cupon1.setSaldo(0);
         cupon2.setSaldo(0);
         return cuponNuevo;
     }
->>>>>>> 64c5f1ab781b77dd8f918cd4436e6a8d1de7a1d8
+*/
 	
 	
 	
--- ProyectoAmazon/src/Main.java
+++ ProyectoAmazon/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		Cupon  c5 = new Cupon(50.0);
 		Cupon  c6 = new Cupon(50.0);
 		Cupon  c7 = new Cupon(50.0);
-		
+		/*
 		System.out.println(c1.getIdentificador());
 		System.out.println(c2.getIdentificador());
 		System.out.println(c3.getIdentificador());
@@ -18,6 +18,21 @@ public class Main {
 		System.out.println(c5.getIdentificador());
 		System.out.println(c6.getIdentificador());
 		System.out.println(c7.getIdentificador());
+		*/
+		/*
+		System.out.println(c1.getSaldo());
+		
+		System.out.println(c2.getSaldo());
+		Cupon nuevo = Cupon.fusionarCupones(c1, c2);
+		System.out.println(c1.getSaldo());
+		
+		System.out.println(c2.getSaldo());
+		System.out.println(nuevo.getSaldo());
+		*/
+		c1.añadirSaldo(10);
+		c1.gastarSaldo1(10);
+		Cupon.mostrarInformacion(c1);
+		
 		
 	}
 
